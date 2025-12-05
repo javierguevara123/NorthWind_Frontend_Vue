ddebf39737c53988434994b92f07b3cf3b78d179
Javier Guevara <jefferguevara2003@gmail.com>
2025-12-05 15:34:01 -0500

    10. Guevara

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -140,7 +140,7 @@ export default {
 
       // 3. Petición al Backend
       try {
-        const response = await fetch("http://northwindweb.somee.com/user/Login", {
+        const response = await fetch("/api/user/Login", {
           method: "POST",
           headers: { 
             "Content-Type": "application/json",
